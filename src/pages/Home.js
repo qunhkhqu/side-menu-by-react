b9d6f1c8612fc47dbb303810ee0f3f5d4e53e8b3
quangnt <nguyentienquangttm@gmail.com>
2021-10-19 18:44:47 +0700

    update: use react hook example

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-function MouseClick()
-{
+
+function MouseClick() {
     const [click, setClick] = useState(0)
 
-    function incrementClick()
-    {
-        setClick(click+1);
+    function incrementClick() {
+        setClick(click + 1);
     }
 
     // useEffect(()=>{
@@ -14,18 +13,76 @@ function MouseClick()
 
     // });
 
-    useEffect(()=>{
-        console.log("Click: "+ click);
+    // useEffect(() => {
+    //     console.log("Click: " + click);
+    // }, [click]);
 
-    },[click]);
-
-    return(
+    return (
         <div className="count-click">
-            <button onClick={incrementClick}>Click Here!</button>
+            <button onClick={incrementClick}>Click to count:</button>
             <span>{click}</span>
         </div>
     )
 }
+
+
+
+class Timer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { tick: 0 }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.setState({ tick: this.state.tick + 1 }), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return (
+            <div className="timer">
+                Time in seconds: <span> {this.state.tick} </span>
+            </div>
+        )
+    }
+}
+
+
+function TimerByFuncComponent() {
+
+    const [tick, setTick] = useState(0);
+
+    const refTick = useRef(0);
+
+    useEffect(() => {
+
+        console.log('->Khoi tao timmer');
+        var count = 0;
+        const i = setInterval(() => {
+            //console.log("Tick: " + count);
+            let newtick = refTick.current + 1;
+            count++;
+            setTick(newtick);
+            refTick.current = newtick;
+        }, 1000);
+
+        return () => {
+            clearInterval(i);
+        }
+
+    }, []);
+
+    return (
+        <div className="timer">
+            Time in seconds: <span> {tick} </span>
+        </div>
+    );
+}
+
 
 function Home() {
     return (
@@ -33,10 +90,19 @@ function Home() {
             <div className='main'>
                 <h1>Home</h1>
                 <div className="count-click-container">
-                    <MouseClick/>
-                    <MouseClick/>
-                    <MouseClick/>
+                    <MouseClick />
+                    <MouseClick />
+                    <MouseClick />
                 </div>
+
+                <div className="count-click-container">
+                    <Timer />
+                </div>
+
+                <div className="count-click-container">
+                    <TimerByFuncComponent />
+                </div>
+
             </div>
         </>
     )
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+function TodoApp() {
+    return (
+        <div>
+            <h4>List of thing you want to do</h4>
+
+        </div>
+    )
+}
+
+
 function Products() {
     return (
         <div className='main'>
             <h1>Products</h1>
+
+            <div className="sub-app">
+                <TodoApp/>
+            </div>
+
         </div>
     )
 }
