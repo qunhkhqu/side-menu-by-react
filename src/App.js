f11cb4554dee00c51e48916b3a81fa78f77b7830
quangnt <nguyentienquangttm@gmail.com>
2023-06-28 18:09:39 +0700

    update App

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ import Home from './pages/Home'
 import Reports from './pages/Reports'
 import Products from './pages/Products'
 
+// App
+
 function App() {
   return (
     <>
       <Router>
-          <Navbar/>
-          <Switch>
-            <Route path='/' exact component={Home}/>
-            <Route path='/reports' component={Reports}/>
-            <Route path='/products' component={Products}/>
-          </Switch>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
       </Router>
     </>
   );
